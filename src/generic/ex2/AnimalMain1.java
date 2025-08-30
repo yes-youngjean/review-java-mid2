ff8417f04684f02595ca807529b290875f834c5c
youngjean <yeszerojean@gmail.com>
2025-08-30 14:30:51 +0900

    Lesson: Generic1 - 활용

---
+++ src/generic/ex2/AnimalMain1.java
@@ -0,0 +1,31 @@
+package generic.ex2;
+
+import generic.animal.Animal;
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class AnimalMain1 {
+    public static void main(String[] args) {
+        Animal animal = new Animal("동물", 0);
+        Dog dog = new Dog("멍멍이", 100);
+        Cat cat = new Cat("냐옹이", 50);
+
+        //멍멍이용 Box
+        Box<Dog> dogBox = new Box<>();
+        dogBox.set(dog);
+        Dog findDog = dogBox.get();
+        System.out.println("findDog = " + findDog);
+
+        //냐옹이용 Box
+        Box<Cat> catBox = new Box<>();
+        catBox.set(cat);
+        Cat findCat = catBox.get();
+        System.out.println("findCat = " + findCat);
+
+        //Animal용 Box
+        Box<Animal> animalBox = new Box<>();
+        animalBox.set(animal);
+        Animal findAnimal = animalBox.get();
+        System.out.println("findAnimal = " + findAnimal);
+    }
+}
